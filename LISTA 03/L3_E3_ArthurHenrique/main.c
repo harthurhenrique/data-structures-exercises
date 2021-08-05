3fe81140d5ef39d5f393a8b33b48fc522fd1e448
harthurhenrique <85003038+harthurhenrique@users.noreply.github.com>
2021-08-05 10:20:56 -0300

    Update main.c

--- LISTA 03/L3_E3_ArthurHenrique/main.c
+++ LISTA 03/L3_E3_ArthurHenrique/main.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 
-/*Faça uma função que receba como parâmetro um vetor X de 30 elementos inteiros e retorne,
+/*
+Faça uma função que receba como parâmetro um vetor X de 30 elementos inteiros e retorne,
 também por parâmetro, dois vetores A e B. O vetor A deve conter os elementos pares de X
-e o vetor B, os elementos ı́mpares.*/
+e o vetor B, os elementos ı́mpares.
+*/
 
 void Divide(int *VetX,int n,int *VetA,int *tamA,int *VetB,int *tamB)
 {
