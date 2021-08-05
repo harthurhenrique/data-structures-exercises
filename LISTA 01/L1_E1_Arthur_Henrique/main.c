c78f50105443f6475458e1b8f1dcd4f3f9ba9793
harthurhenrique <85003038+harthurhenrique@users.noreply.github.com>
2021-08-05 10:15:33 -0300

    Update main.c

--- LISTA 01/L1_E1_Arthur_Henrique/main.c
+++ LISTA 01/L1_E1_Arthur_Henrique/main.c
@@ -2,6 +2,16 @@
 #include <string.h>
 #include <stdlib.h>
 
+/*
+Crie uma estrutura representando os alunos de um determinado curso. A estrutura deve conter a
+matr cula do aluno, nome, nota da primeira prova, nota da segunda prova e nota da terceira prova. ı́
+(a) Permita ao usuário entrar com os dados de 5 alunos.
+(b) Encontre o aluno com maior nota da primeira prova.
+(c) Encontre o aluno com maior média geral.
+(d) Encontre o aluno com menor média geral
+(e) Para cada aluno diga se ele foi aprovado ou reprovado, considerando o valor 6 para aprovação.
+*/
+
 struct Aluno{
     char nome [50];
     char mat [30];
