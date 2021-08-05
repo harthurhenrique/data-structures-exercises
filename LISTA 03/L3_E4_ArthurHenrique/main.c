389f066cdb5a9f27ff71b58608fee74307ad30fe
harthurhenrique <85003038+harthurhenrique@users.noreply.github.com>
2021-08-05 10:21:26 -0300

    Update main.c

--- LISTA 03/L3_E4_ArthurHenrique/main.c
+++ LISTA 03/L3_E4_ArthurHenrique/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+/*
+Considerando a estrutura:
+struct Ponto{
+int x;
+int y;
+};
+para representar um ponto em uma grade 2D, implemente uma função que indique se um ponto p
+está localizado dentro ou fora de um retângulo. O retângulo é definido por seus vértices inferior
+esquerdo v1 e superior direito v2. A função deve retornar 1 caso o ponto esteja localizado dentro do
+retângulo e 0 caso contrário. Essa função deve obedecer ao protótipo:
+int dentroRet (struct Ponto* v1, struct Ponto* v2, struct Ponto* p);
+*/
+
 struct Ponto{
     int x;
     int y;
