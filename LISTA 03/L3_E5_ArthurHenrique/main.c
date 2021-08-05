cd683f18bbde3460b41a937f9d27cf845cfbd03b
harthurhenrique <85003038+harthurhenrique@users.noreply.github.com>
2021-08-05 10:21:57 -0300

    Update main.c

--- LISTA 03/L3_E5_ArthurHenrique/main.c
+++ LISTA 03/L3_E5_ArthurHenrique/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+/*
+Considerando a estrutura
+struct Vetor{
+float x;
+float y;
+float z;
+};
+para representar um vetor no R3
+ , implemente uma função que calcule a soma de dois vetores. Essa
+função deve obedecer ao protótipo:
+void soma (struct Vetor* v1, struct Vetor* v2, struct Vetor* res);
+onde os parâmetros v1 e v2 são ponteiros para os vetores a serem somados, e o parâmetro res é um
+ponteiro para uma estrutura vetor onde o resultado da operação deve ser armazenado.
+*/
+
 struct Vetor{
     float x;
     float y;
