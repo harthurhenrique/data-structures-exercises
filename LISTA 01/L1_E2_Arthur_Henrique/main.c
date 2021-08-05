ec3302a82542444006f5959e3b2e2e8993b9beb9
harthurhenrique <85003038+harthurhenrique@users.noreply.github.com>
2021-08-05 10:16:11 -0300

    Update main.c

--- LISTA 01/L1_E2_Arthur_Henrique/main.c
+++ LISTA 01/L1_E2_Arthur_Henrique/main.c
@@ -2,6 +2,14 @@
 #include <string.h>
 #include <stdlib.h>
 
+/*
+Faça um programa que armazene em um registro de dados (estrutura composta) os dados de um
+funcionário de uma empresa, compostos de: Nome, Idade, Sexo (M/F), CPF, Data de Nascimento,
+Código do Setor onde trabalha (0-99), Cargo que ocupa (string de até 30 caracteres) e Salário. Os
+dados devem ser digitados pelo usuário, armazenados na estrutura e exibidos na tela.
+
+*/
+
 struct Funcionario{
     char nome [30];
     int idade;
