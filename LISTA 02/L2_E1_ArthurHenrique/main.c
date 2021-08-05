9ec5e3d41d99525f3f07bfbfb6fa93da97242f31
harthurhenrique <85003038+harthurhenrique@users.noreply.github.com>
2021-08-05 10:17:27 -0300

    Update main.c

--- LISTA 02/L2_E1_ArthurHenrique/main.c
+++ LISTA 02/L2_E1_ArthurHenrique/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+/*
+Crie uma função que receba como parâmetro um valor inteiro e gere como sa da n linhas com ı́
+pontos de exclamação, conforme o exemplo abaixo (para n = 5):
+!
+!!
+!!!
+!!!!
+!!!!!
+*/
+
 void exclama(int n){
     for(int i=0;i<n;i++){
         printf("!");
